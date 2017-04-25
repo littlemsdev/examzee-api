0fd8fe59aa066f095769de055e0b0c5fdb358cef
chataw <tawcharlene@gmail.com>
2017-04-26 00:42:09 +0800

    fixed double comma

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username,, :password,, :full_name)
+      params.require(:user).permit(:username, :password, :full_name)
     end
 end
