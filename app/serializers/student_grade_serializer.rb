b3d37ef6249991be4090a5189bb2ef0ebc642778
chataw <tawcharlene@gmail.com>
2017-04-26 15:13:30 +0800

    add string status grade

--- app/serializers/student_grade_serializer.rb
+++ app/serializers/student_grade_serializer.rb
@@ -1,5 +1,5 @@
 class StudentGradeSerializer < ActiveModel::Serializer
-  attributes :id, :student_id, :student_number, :exam_id, :answer_keys, :status, :exam_name
+  attributes :id, :student_id, :student_number, :exam_id, :answer_keys, :status, :exam_name, :status_grade
 
   def status_grade
     case object.status
